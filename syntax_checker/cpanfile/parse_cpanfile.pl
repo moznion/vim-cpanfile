3ae16a51edf799ce691410f4366fad2593f17e68
moznion <moznion@gmail.com>
2013-04-15 02:29:32 +0900

    Implement basic syntax_checker

---
+++ syntax_checker/cpanfile/parse_cpanfile.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+
+use Module::CPANfile;
+
+my $file = $ARGV[0];
+eval { Module::CPANfile->load($file) };
+my $res = $@;
+
+my $type = 'E';    # <= Means Error
+my ( $file_name, $line, $err_msg ) =
+  $res =~ /failed: syntax error at (.*?) line (\d*?), (.*)/;
+$err_msg = 'syntax error: ' . $err_msg;
+
+print "$type;$file_name;$line;$err_msg";
